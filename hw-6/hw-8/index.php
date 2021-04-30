c53e67b0b26b57b9fefb1d3491677d735c67ed47
IsakovaL <helis.riba@gmail.com>
2021-04-30 23:53:00 +0300

    one more try to fix

--- hw-6/hw-8/index.php
+++ hw-6/hw-8/index.php
@@ -1,18 +1,22 @@
-<?php
 
-echo "Give me a number:\n";
+    <?php
+
+echo "Give me a number less than 18:\n";
 
 $handle = fopen("php://stdin","r");
-$fizz = (int)fgets($handle);
-$buzz = (int)fgets($handle);
-$number = (int)fgets($handle);
+$fizz = fgets($handle);
+$buzz = fgets($handle);
+$number = fgets($handle);
 
 echo "\n";
 
-if ($number % 3 == 0) {
+$i = 0;
+for($i = 0; $i < 18; $i++){
+if ($i % 18 == 0) {
     echo("F\n");
-    } elseif($number % 5 == 0) {
+    } elseif($i % 3 == 0) {
         echo("B\n");
-    } elseif($fizz  % 3 == 0 && $buzz % 5 == 0) {
+    } elseif($i % 5 == 0 && $i % 5 == 0) {
             echo("FB\n");
-    }  else var_dump ($number);
+    }  else var_dump ($i);
+};
