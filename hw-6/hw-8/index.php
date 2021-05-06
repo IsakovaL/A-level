38b26cfb55b3263c74e14dcbd136e0a53d6d376f
IsakovaL <helis.riba@gmail.com>
2021-05-06 18:51:44 +0300

    final fix

--- hw-6/hw-8/index.php
+++ hw-6/hw-8/index.php
@@ -1,5 +1,4 @@
-
-    <?php
+<?php
 
 echo "Give me a number\n";
 
@@ -10,13 +9,14 @@ $number = (int)fgets($handle);
 
 echo "\n";
 
-$i = 0;
-for($i = 0; $i < $number; $i++){
-if ($i % $fizz== 0) {
-    echo("F\n");
-    } elseif($i % $buzz== 0) {
-        echo("B\n");
-    } if($i % (int)$fizz == 0 && $i % (int)$buzz == 0) {
-            echo("FB\n");
+$i = 1;
+for($i = 1; $i <= $number; $i++) {
+
+    if($i % $fizz == 0 && $i % $buzz == 0) {
+        echo("FB\n"); 
+        } elseif($i % $fizz == 0) {
+            echo("F\n");
+        } elseif($i % $buzz == 0) {
+            echo("B\n");
+        }  else echo("$i\n");
     };
-};
