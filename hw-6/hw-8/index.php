70d5cd616a3f78c27be2643b2f831a757bdf890c
IsakovaL <helis.riba@gmail.com>
2021-05-06 13:37:43 +0300

    fixed hw

--- hw-6/hw-8/index.php
+++ hw-6/hw-8/index.php
@@ -1,22 +1,22 @@
 
     <?php
 
-echo "Give me a number less than 18:\n";
+echo "Give me a number\n";
 
 $handle = fopen("php://stdin","r");
-$fizz = fgets($handle);
-$buzz = fgets($handle);
-$number = fgets($handle);
+$fizz = (int)fgets($handle);
+$buzz = (int)fgets($handle);
+$number = (int)fgets($handle);
 
 echo "\n";
 
 $i = 0;
-for($i = 0; $i < 18; $i++){
-if ($i % 18 == 0) {
+for($i = 0; $i < $number; $i++){
+if ($i % $fizz== 0) {
     echo("F\n");
-    } elseif($i % 3 == 0) {
+    } elseif($i % $buzz== 0) {
         echo("B\n");
-    } elseif($i % 5 == 0 && $i % 5 == 0) {
+    } if($i % (int)$fizz == 0 && $i % (int)$buzz == 0) {
             echo("FB\n");
-    }  else var_dump ($i);
+    };
 };
