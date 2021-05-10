3926947915368682c70977f124fe2651d5044d3a
IsakovaL <helis.riba@gmail.com>
2021-05-11 02:22:46 +0300

    no comments

--- homework/hw-9/index.php
+++ homework/hw-9/index.php
@@ -6,51 +6,97 @@
 // Имена файлов написать в комментариях в коде программы. Действия оформить в две функции: 
 // одна для прочтения, другая для записи. Параметром обеих функций долен быть путь к файлу.
 
-function fileread() {
-    
-    $file_array = file("/home/isakoval/Desktop/A-level/hw-6/hw-9/text.txt"); //Считывание файла в массив $file_array
- 
-    $newfilepass = "/home/isakoval/Desktop/A-level/hw-6/hw-9/newtext.txt";
+$new = "";
 
-    $newfile = ""; //создаем файл
+    $file_path = "/home/isakoval/Desktop/A-level/homework/hw-9/text.txt";
+    //$new_file_path = "/home/isakoval/Desktop/A-level/homework/hw-9/newtext.txt";
 
-        if (!file_exists($newfilepass)) {  //if no such file 
+    function file_read($file_path) {
+
+        $open_file = fopen($file_path, "r+");
+
+            while(!feof($open_file)) {
+
+                $new_file = fgets($open_file);
+            }
+                fclose($open_file);
+    };
+
+    function file_write($file_path,$new) {
+
+        $open_file = fopen($file_path, "r+");
+
+        $new_file = fgets($open_file);
+
+               
                 
-            $newfile = fopen($newfilepass, "w"); //создаем файл
+            for($i=0; $i < strlen($new_file); $i +=2) {
 
-               // $newfileArr = fopen($newfilepass, "a+");// Открываем файл в режиме записи
-            };
+                $new = fopen($new, "a+");
+
+                fwrite($open_file, $new_file);
+                
+               
+            }
+
+                fclose($open_file);
+                fclose($file_path);
+
+    };
+    file_read($file_path);
+    file_write($new);
+
+
+
+
+
+
+//         if (!file_exists($newfilepath)) {  //if no such file 
+                
+//             $newfile = fopen($newfilepath, "w"); //создаем файл
+
+//                $newfileArr = fgets($newfilepath, "a+");// Открываем файл в режиме записи
+//             };
          
-        for($i = 0; $i < count($file_array); $i+=2) {
+//         for($i = 0; $i < strlen($file_array); $i+=2) {
 
-            print_r("hello");
+//             print_r("hello");
 
-            $string = $file_array[$i];
+//             $string = $file_array[$i];
 
-            $newarray = fwrite($newfile, $string);// Запись в файл
-        };
-        
-    // fclose($newfile);
-};
+//             $newarray = fwrite($newfile, $string);// Запись в файл
+//         };
+//     };
 
-fileread("text.txt");
+// fileread();
 
 //Функцией прочесть из файла все строки. 
 //Другой функцией померять длину каждой строки. 
 //Третьей функцией записать в совершенно другой файл только те строки, которые длиннее средней длины по файлу.
 
+$file_path = "/home/isakoval/Desktop/A-level/homework/hw-9/text.txt";
 
-
-// function readAllStrings() {
+ function readAllStrings() {
     
-//         $file = fopen('/home/isakoval/Desktop/A-level/hw-6/hw-9/newtext.txt', 'r+');
+    $opn_file = fopen($file_path, 'r+');
         
-//         $newArr = file('/home/isakoval/Desktop/A-level/hw-6/hw-9/newtext.txt');
+        while(!feof($opn_file)) {
 
-//         while(!feof($file)) {
-//             $a = fgets($file);
-//         };
-//     fclose($file);
+            $read_file = fgets($opn_file);
 
-//     }
-//   readAllStrings();
+        }
+    fclose($opn_file);
+
+ };
+
+
+    $file_path = "/home/isakoval/Desktop/A-level/homework/hw-9/text.txt";
+    $strings_array = file($file_path);
+
+    foreach ($strings_array as $key => $str) {
+   
+        $
+    };
+
+    // echo strlen($opn_file);
+
