1c898cbd91960809293d255d00d665a6f439fe33
IsakovaL <helis.riba@gmail.com>
2021-06-07 21:54:29 +0300

    commit

---
+++ lessons/js-1/script.js
@@ -0,0 +1,63 @@
+// let obj = {
+//     name: 'Alex',
+//     age: 30,
+//     lan: ['en', 'ru']
+// }
+//
+// console.log(typeof obj)
+
+// let a = 4
+// let b = 5
+// console.log(a += b)
+
+// console.log(0.2 + 0.4)
+
+let a = 2
+let b = 3
+console.log(a+++b)
+
+
+// let ocenka = 2
+// let res = ocenka > 2 ? 'passed exam' : 'didnt pass exam'
+// console.log(res)
+
+// let a = 10
+// let b = a++
+//
+// console.log('a = ', a)
+// console.log('b = ', b)
+
+// console.log(`result is: ${1 + 2}`)
+
+// let isGreater = 4 < 1
+// console.log(isGreater)
+
+// let age = null
+// console.log(age)
+//
+// let x
+// console.log(x)
+
+// let firstName = "elena";
+//
+// const AGE = 38;
+//
+// let result = firstName + AGE;
+//
+// alert(result);
+// alert(AGE + ` ` + firstName);
+// alert (`${AGE} ${firstName}`);
+
+// let age = prompt("how old are you?")
+//
+// alert(age);
+//
+// let user = "John";
+// let age = 25;
+// let message  = "Hello";
+//
+// console.log(`${user} ${age} ${message}`)
+//
+// let $ = 1
+// let _ = 2
+// console.log($ + _)
